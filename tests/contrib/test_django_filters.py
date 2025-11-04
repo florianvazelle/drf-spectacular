florianvazelle/drf-spectacular

commit 2c71a35c5d11809f2c06b85ccb8d83b541f4f92c

[django-filter] Add null_label if set in ChoiceFilter (#1450)
* Add null_label if set in ChoiceFilter

* Fix whitespace

* Replace null_label for null_value + add empty_label management

* Remove empty_label

---------

Co-authored-by: T. Franzel <tfranzel@users.noreply.github.com>

drf_spectacular/contrib/django_filters.py
@@ -228,7 +228,12 @@ class DjangoFilterExtension(OpenApiFilterExtension):
             # choices function may utilize the DB, so refrain from actually calling it.
             return []
         else:
-            return [c for c, _ in filter_field.extra['choices']]
+            choices = [c for c, _ in filter_field.extra['choices']]
+
+            if filter_field.field.null_label:
+                choices.append(filter_field.field.null_value)
+
+            return choices
 
     def _get_model_field(self, filter_field, model):
         if not filter_field.field_name:

tests/contrib/test_django_filters.py
@@ -4,6 +4,7 @@ import pytest
 from django import __version__ as DJANGO_VERSION
 from django.db import models
 from django.db.models import F
+from django.test import override_settings
 from django.urls import include, path
 from rest_framework import generics, routers, serializers, viewsets
 from rest_framework.test import APIClient
@@ -450,3 +451,31 @@ def test_filter_on_listapiview(no_warnings):
 
     schema = generate_schema('/x/', view=XListView)
     assert len(schema['paths']['/x/']['get']['parameters']) > 1
+
+
+@pytest.mark.contrib('django_filter')
+@override_settings(
+    FILTERS_NULL_CHOICE_VALUE="NULL VALUE",
+)
+def test_filterset_enum_includes_null_label(no_warnings):
+    class SimpleModelFilterSet(FilterSet):
+        class Meta:
+            model = SimpleModel
+            fields = ("category",)
+
+        category = ChoiceFilter(
+            choices=(('a', 'A'), ('b', 'B')),
+            null_label="NULL LABEL"
+        )
+
+    class XViewSet(viewsets.ModelViewSet):
+        queryset = SimpleModel.objects.all()
+        serializer_class = SimpleSerializer
+        filter_backends = [DjangoFilterBackend]
+        filterset_class = SimpleModelFilterSet
+
+    schema = generate_schema('/x', XViewSet)
+    category_type_schema = schema['paths']['/x/']['get']['parameters'][0]
+
+    assert category_type_schema['name'] == 'category'
+    assert category_type_schema['schema']['enum'] == ["NULL VALUE", "a", "b"]

commit 02971bd89e25e0a3cda5f94400ab497f20112f7d

Fix regression introduced in #1450 (#1469)
* chore: extend test to reproduce the issue

Signed-off-by: Oleg Hoefling <oleg.hoefling@gmail.com>

* fix(django-filters): handle the null value of a choice filter in a duck type manner

Signed-off-by: Oleg Hoefling <oleg.hoefling@gmail.com>

* chore: fix flake8 issues

Signed-off-by: Oleg Hoefling <oleg.hoefling@gmail.com>

---------

Signed-off-by: Oleg Hoefling <oleg.hoefling@gmail.com>

drf_spectacular/contrib/django_filters.py
@@ -230,7 +230,7 @@ class DjangoFilterExtension(OpenApiFilterExtension):
         else:
             choices = [c for c, _ in filter_field.extra['choices']]
 
-            if filter_field.field.null_label:
+            if getattr(filter_field.field, 'null_label', None) is not None:
                 choices.append(filter_field.field.null_value)
 
             return choices

tests/contrib/test_django_filters.py
@@ -18,7 +18,8 @@ try:
     from django_filters.rest_framework import (
         AllValuesFilter, BaseInFilter, BooleanFilter, CharFilter, ChoiceFilter, DjangoFilterBackend,
         FilterSet, ModelChoiceFilter, ModelMultipleChoiceFilter, MultipleChoiceFilter, NumberFilter,
-        NumericRangeFilter, OrderingFilter, RangeFilter, UUIDFilter,
+        NumericRangeFilter, OrderingFilter, RangeFilter, TypedChoiceFilter,
+        TypedMultipleChoiceFilter, UUIDFilter,
     )
 except ImportError:
     class DjangoFilterBackend:  # type: ignore
@@ -458,24 +459,42 @@ def test_filter_on_listapiview(no_warnings):
     FILTERS_NULL_CHOICE_VALUE="NULL VALUE",
 )
 def test_filterset_enum_includes_null_label(no_warnings):
-    class SimpleModelFilterSet(FilterSet):
+    class ProductFilterSet(FilterSet):
         class Meta:
-            model = SimpleModel
+            model = Product
             fields = ("category",)
 
         category = ChoiceFilter(
             choices=(('a', 'A'), ('b', 'B')),
             null_label="NULL LABEL"
         )
+        category_typed = TypedChoiceFilter(
+            choices=(('a', 'A'), ('b', 'B')),
+            field_name='category'
+        )
+        category_typed_multi = TypedMultipleChoiceFilter(
+            choices=(('a', 'A'), ('b', 'B')),
+            field_name='category'
+        )
 
     class XViewSet(viewsets.ModelViewSet):
-        queryset = SimpleModel.objects.all()
-        serializer_class = SimpleSerializer
+        queryset = Product.objects.all()
+        serializer_class = ProductSerializer
         filter_backends = [DjangoFilterBackend]
-        filterset_class = SimpleModelFilterSet
+        filterset_class = ProductFilterSet
 
     schema = generate_schema('/x', XViewSet)
     category_type_schema = schema['paths']['/x/']['get']['parameters'][0]
 
     assert category_type_schema['name'] == 'category'
     assert category_type_schema['schema']['enum'] == ["NULL VALUE", "a", "b"]
+
+    category_typed_type_schema = schema['paths']['/x/']['get']['parameters'][1]
+
+    assert category_typed_type_schema['name'] == 'category_typed'
+    assert category_typed_type_schema['schema']['enum'] == ["a", "b"]
+
+    category_typed_multi_type_schema = schema['paths']['/x/']['get']['parameters'][2]
+
+    assert category_typed_multi_type_schema['name'] == 'category_typed_multi'
+    assert category_typed_multi_type_schema['schema']['items']['enum'] == ["a", "b"]